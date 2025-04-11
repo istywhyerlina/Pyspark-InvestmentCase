4a8f2b6b19263c9af02e88a365296c8b996379c2
istywhyerlina <istywahyuerlina@gmail.com>
2025-04-11 12:45:56 +0700

    add staging.sql for initiation database

--- traget_db/init.sql
+++ traget_db/init.sql
@@ -1,2 +1,3 @@
-CREATE DATABASE staging;
-CREATE DATABASE etl_log;
+version https://git-lfs.github.com/spec/v1
+oid sha256:0dbca880e18ee694a95934af42076e438f15bea44e9efa819d32591c7dc4adca
+size 24
---
+++ traget_db/staging.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2268d72151e19f42f708fef9bfc886430564c56f358b530b7331d0a047acaf15
+size 7930
