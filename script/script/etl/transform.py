d49e484d88d7d9c13b74bedac6f2a7eae4df58a0
istywhyerlina <istywahyuerlina@gmail.com>
2025-05-02 09:38:36 +0700

    Transform class for dimension tables

--- script/script/etl/transform.py
+++ script/script/etl/transform.py
@@ -18,9 +18,11 @@ import pandas as pd
 from pyspark.sql.functions import to_timestamp
 from pyspark.sql.functions import to_date
 from pyspark.sql.functions import *
+from script.helper.log_success import log_success
+from script.helper.log_error import log_error
 
 class Transform():
-    def __init__:
+    def __init__(self):
         self.stg_people=extract_db_stg("people")
         self.stg_relationships=extract_db_stg("relationships")
         self.stg_acquisition=extract_db_stg("acquisition")
@@ -30,7 +32,51 @@ class Transform():
         self.stg_ipos=extract_db_stg("ipos")
         self.stg_funding_rounds=extract_db_stg("funding_rounds")
         
-    def dim_table():
-        self.dim_term_code=self.stg_acquisition.select(self.stg_acquisition['term_code']).distinct()
-        self.stock_symbol=self.stg_ipos.select(self.stg_ipos['stock_symbol']).distinct()
+    def dim_term_code(self):
+        step="Data Warehouse"
+        process="Transform"
+        try:
+            self.dim_term_code=self.stg_acquisition.select(self.stg_acquisition['term_code']).distinct()
+            log_success(step,process,"Acquisition","dim_term_code")
+            return self.dim_term_code
+        except Exception as e:
+            log_error(step,process,"Acquisition","dim_term_code",str(e))
+
+    def dim_stock_symbol(self):
+        step="Data Warehouse"
+        process="Transform"
+        try:
+            self.dim_stock_symbol=self.stg_ipos.select(self.stg_ipos['stock_symbol']).distinct()
+            log_success(step,process,"Ipos","dim_stock_symbol")
+            return self.dim_stock_symbol
+        except Exception as e:
+            log_error(step,process,"Ipos","dim_stock_symbol",str(e))
+    
+    def dim_company(self):
+        step="Data Warehouse"
+        process="Transform"
+        try:
+            RENAME_COLS = {"office_id": "company_nk_id"}
+            self.dim_company = self.stg_company.withColumnsRenamed(colsMap = RENAME_COLS)
+            columns_to_drop = ['created_at', 'updated_at']
+            self.dim_company = self.dim_company.drop(*columns_to_drop)
+            log_success(step,process,"Company","dim_company")
+            return self.dim_company
+        except Exception as e:
+            log_error(step,process,"Company","dim_company",str(e))
+    
+    def dim_people(self):
+        step="Data Warehouse"
+        process="Transform"
+        try:
+            RENAME_COLS = {"people_id": "people_nk_id"}
+            self.dim_people = self.stg_people.withColumnsRenamed(colsMap = RENAME_COLS)
+            columns_to_drop = ['created_at', 'updated_at']
+            self.dim_people = self.dim_people.drop(*columns_to_drop)
+            self.dim_people=self.dim_people.withColumn("people_nk_id",dim_people ["people_nk_id"].cast("int"))
+            log_success(step,process,"People","dim_people")
+            return self.dim_people
+        except Exception as e:
+            log_error(step,process,"People","dim_people",str(e))
+
 
